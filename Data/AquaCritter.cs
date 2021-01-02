1a54fd2fd7985809d1178c3d0aa41734c331d584
Joe Baranski <joe.baranski49@gmail.com>
2021-01-02 14:05:50 -0500

    first commit

---
+++ Data/AquaCritter.cs
@@ -0,0 +1,9 @@
+namespace blazoraqua.Data
+{
+  public class AquaCritter
+  {
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public bool isDead { get; set; }
+  }
+}
